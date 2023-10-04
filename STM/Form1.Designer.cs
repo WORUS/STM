cefc3c2a4bdc53a8ecf05842e0a0005dada45fdd
WORUS <aswoks@gmail.com>
2023-10-04 12:57:11 +0400

    Добавьте файлы проекта.

---
+++ STM/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace STM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.StripLine stripLine2 = new System.Windows.Forms.DataVisualization.Charting.StripLine();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.MistyRose;
+            chartArea2.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea2.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.AxisX.LabelAutoFitMaxFontSize = 7;
+            chartArea2.AxisX.LabelAutoFitMinFontSize = 5;
+            chartArea2.AxisX.LabelStyle.Enabled = false;
+            chartArea2.AxisX.LabelStyle.Interval = 0D;
+            chartArea2.AxisX.LineColor = System.Drawing.Color.Transparent;
+            chartArea2.AxisX.LineWidth = 2;
+            chartArea2.AxisX.MajorGrid.Enabled = false;
+            chartArea2.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea2.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea2.AxisY.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisY.IsLabelAutoFit = false;
+            chartArea2.AxisY.LabelStyle.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea2.AxisY.LabelStyle.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea2.AxisY.LabelStyle.TruncatedLabels = true;
+            chartArea2.AxisY.LineColor = System.Drawing.Color.Transparent;
+            chartArea2.AxisY.MajorGrid.Enabled = false;
+            chartArea2.AxisY.StripLines.Add(stripLine2);
+            chartArea2.AxisY.TitleForeColor = System.Drawing.Color.Transparent;
+            chartArea2.BackColor = System.Drawing.Color.White;
+            chartArea2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Alignment = System.Drawing.StringAlignment.Center;
+            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(189, 41);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.IsVisibleInLegend = false;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            series7.BorderWidth = 3;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.IsVisibleInLegend = false;
+            series7.Legend = "Legend1";
+            series7.Name = "Series2";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.IsVisibleInLegend = false;
+            series8.Legend = "Legend1";
+            series8.Name = "Series3";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.IsVisibleInLegend = false;
+            series9.Legend = "Legend1";
+            series9.Name = "Series4";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series10.IsVisibleInLegend = false;
+            series10.Legend = "Legend1";
+            series10.Name = "Series5";
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Size = new System.Drawing.Size(587, 365);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title2.Name = "Сканирующий туннельный микроскоп";
+            this.chart1.Titles.Add(title2);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(701, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "U = 0.1 В";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 325);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "5A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "7A";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "10A";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "15A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "U = ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 180);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(788, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Сканирующий туннельный микроскоп";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ STM/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace STM
+{
+    public partial class Form1 : Form
+    {
+        public static int[] place = new int[16] { 0, 0, 10, 0, 10, 5, 20, 5, 20, 0, 25, 0, 25, 7, 35, 7};
+        
+        RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+        public static readonly int N = 1000;
+
+        public static byte[] rngBytes = new byte[2];
+
+        public static double u = 0.1, ef = 5.71, fi = 4.5, k = 1;
+
+        public static double[] z0 = new double[4] { 5, 7, 10, 15 };
+
+        public double I_et = 0;
+
+        public double delta = 0.02, EPS = 0.001;
+
+        public int line_count;
+
+        public double rangeY_integral = 10, rangeX_integral = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct Line
+        {
+            public double dx, dy, x0, y0, x1, y1, Length, nX, nY;
+        }
+
+        public Line[] Line_ = new Line[11];
+
+        public void SetLine(double x0, double y0, double x1, double y1, int counter)
+        {
+            Line_[counter].dx = x1 - x0;
+            Line_[counter].dy = y1 - y0;
+            Line_[counter].x0 = x0;
+            Line_[counter].x1 = x1;
+            Line_[counter].y0 = y0;
+            Line_[counter].Length = Math.Sqrt(Line_[counter].dx * Line_[counter].dx + Line_[counter].dy * Line_[counter].dy);
+            Line_[counter].dx /= Line_[counter].Length;
+            Line_[counter].dy /= Line_[counter].Length;
+            Line_[counter].nX = -Line_[counter].dy;
+            Line_[counter].nY = Line_[counter].dx;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public double J(double x, double y, double z) {
+            double zxy, s1, s2, fi_s;
+            zxy = Math.Sqrt(x * x + y * y + z * z);
+            s1 = 3 / (k * fi);
+            s2 = zxy * (1 - 23 / (3 * fi * k * zxy + 10 - 2 * u * k * zxy)) + s1;
+            fi_s = fi - (u * (s1 + s2) / (2 * zxy))
+                - (2.86 / (k * (s2 - s1)))
+                * Math.Log(s2 * (zxy - s2) / (s1 * (zxy - s2)));
+            return 1620 * u * ef * Math.Exp(-1.025 * zxy * Math.Sqrt(fi_s));
+        }
+
+        public double Integrals(double xi, double yi) {
+            double sum = 0;
+            double z, x1, x2;
+            for (int i = 0; i < line_count; i++) 
+            {
+                double d = (xi - Line_[i].x0) * Line_[i].nX + (yi - Line_[i].y0) * Line_[i].nY;
+                if (d > 0)
+                {
+                    z = ((xi - d * Line_[i].nX) - Line_[i].x0) * Line_[i].dx + ((yi - d * Line_[i].nY) - Line_[i].y0) * Line_[i].dy;
+                    if (z + rangeX_integral > Line_[i].Length) x1 = Line_[i].Length;
+                    else x1 = z + rangeX_integral;
+                    x2 = (z - rangeX_integral > 0) ? z - rangeX_integral : 0;
+                    x1 -= z;
+                    x2 -= z;
+                    if (x2 < x1) sum += GetIntegral(d, x2, x1);
+                    //if(line_count > 1)Console.WriteLine($"Ток {i} -й линии: {GetIntegral(d, x2, x1)}");
+                }
+
+            }
+            return sum;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public double GetIntegral(double z, double x_min, double x_max) {
+            double sum = 0, x, y, rng_coef;
+            int errorCounter = 0;
+            for (int i = 0; i < N; i++) {
+                rng.GetBytes(rngBytes);
+                rng_coef = rngBytes[0];
+                rng_coef /= 255;
+                x = x_min + (x_max - x_min) * rng_coef;
+                rng_coef = rngBytes[1];
+                rng_coef /= 255;
+                y = rangeY_integral * rng_coef;
+                if (x < x_min || x > x_max) errorCounter++;
+                else
+                {
+                    sum += J(x, y, z);
+                }
+            }
+            return sum* (x_max - x_min) * rangeY_integral / N;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[1].Points.Clear();
+            chart1.Series[2].Points.Clear();
+            chart1.Series[3].Points.Clear();
+            chart1.Series[4].Points.Clear();
+            button1.Enabled = false;
+            if (comboBox1.SelectedIndex == 0) u = 0.01;
+            if (comboBox1.SelectedIndex == 1) u = 0.1;
+            double b = 35, h = 0.5;
+            double delt = 0.01; 
+            this.chart1.Invoke((MethodInvoker)delegate 
+            {
+                Profilogram1(b, z0[0], Integrals(0, z0[0]), delt, h, 1);
+            });
+        }
+
+
+        public async void Profilogram1(double b, double zh, double I_et, double delt, double h, int index) {
+            double Ik, zh2 = zh, Ik2;
+            byte sign;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            for (double i = 0; i < b; i += h)
+            {
+                Ik = Integrals(i, zh2);
+                if (Ik > I_et)
+                {
+                    zh2 = zh + delt;
+                    sign = 0;
+                }
+                else
+                {
+                    zh2 = zh - delt;
+                    sign = 1;
+                }
+                Ik2 = Integrals(i, zh2);
+                if (Ik2 > I_et && sign == 0)
+                {
+                    while(Integrals(i, zh2)> I_et) zh2 = zh2 + delt;
+                    //zh2 = zh2 + (Ik2 - I_et) * (zh2 - zh) / (Ik2 - Ik);
+                    //Ik2 = Integrals(i, zh2);
+                }
+                else if (Ik2 < I_et && sign == 1)
+                {
+                    while (Integrals(i, zh2) < I_et) zh2 = zh2 - delt;
+                    //zh2 = zh2 + (Ik2 - I_et) * (zh2 - zh) / (Ik2 - Ik);
+                    //Ik2 = Integrals(i, zh2);
+                }
+                chart1.Series[index].Points.AddXY(i, zh2);
+                zh = zh2;
+                await Task.Delay(1);
+            }
+            if (index < 4)
+            {
+                this.chart1.Invoke((MethodInvoker)delegate
+                {
+                    Profilogram1(b, z0[index], Integrals(0, z0[index]), delt, h, index + 1);
+                });
+            }
+            if (index == 3)
+            {
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+
+        }
+
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            this.chart1.Series[0].BorderWidth = 3;
+            this.chart1.ChartAreas[0].AxisY.Maximum = 30;
+            this.chart1.Series[0].Color = Color.Black;
+
+            chart1.Series[1].BorderWidth = 2;
+            chart1.Series[1].Color = Color.Black;
+            chart1.Series[2].BorderWidth = 2;
+            chart1.Series[2].Color = Color.Black;
+            chart1.Series[3].BorderWidth = 2;
+            chart1.Series[3].Color = Color.Black;
+            chart1.Series[4].BorderWidth = 2;
+            chart1.Series[4].Color = Color.Black;
+            chart1.Series[0].Name = "Поверхность";
+            chart1.Series[1].Name = "5A";
+            chart1.Series[2].Name = "7A";
+            chart1.Series[3].Name = "10A";
+            chart1.Series[4].Name = "15A";
+
+            comboBox1.Items.Add("0.01");
+            comboBox1.Items.Add("0.1");
+
+            for (int i = 0; i < 8; i++)
+            {
+                this.chart1.Series[0].Points.AddXY(place[i * 2], place[i * 2 + 1]);
+                if (i > 0) SetLine(place[(i - 1) * 2], place[(i - 1) * 2 + 1], place[i * 2], place[i * 2 + 1], i - 1);
+                line_count++;
+            }
+        }
+    }
+}
